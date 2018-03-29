300f137921fc5b5d5a7858a5d5a28b6b7fa7edaf
didier <didier@d-defrance.fr>
2018-03-30 00:01:25 +0200

    nouvelle version

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -1,6 +1,6 @@
 cd /d "%~dp0"
 git add *
 git commit -a -m "nouvelle version"
-git remote add gitHubOriginLilleSql https://didier-tp:pwd007!@github.com/didier-tp/lille_sql.git
+REM git remote add gitHubOriginLilleSql https://didier-tp:password@github.com/didier-tp/lille_sql.git
 git push -u gitHubOriginLilleSql master
 pause
